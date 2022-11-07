9b311995240626e73e901aee1dfc36c776efec05
Andre Carmona <andre.carmona@ymail.com>
2022-11-07 19:50:23 -0300

    tela de cadastro ok

---
+++ src/app/tela-cadastro/tela-cadastro.component.js
@@ -0,0 +1,7 @@
+function marcar() {
+    var marcar = window.document.querySelector('div.continue-button')
+    marcar.style.textAlign = 'Center'
+    marcar.style.color = 'green'
+    window.alert ('Consulta efetuada com sucesso')
+    window.location.reload(true);
+}
